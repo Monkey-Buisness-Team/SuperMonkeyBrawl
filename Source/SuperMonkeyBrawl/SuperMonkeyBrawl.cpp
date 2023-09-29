79cce67d7f88f31d565216f09ee798496630bdd7
rBLOT-Arcenz <tsuku0611@protonmail.com>
2023-09-29 17:56:44 +0200

    Add Unreal Engine
    Add Unreal Engine Project

---
+++ Source/SuperMonkeyBrawl/SuperMonkeyBrawl.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SuperMonkeyBrawl.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SuperMonkeyBrawl, "SuperMonkeyBrawl" );
