5fafbb3110f4b98b4b505d720dae3c281da9ee51
Ryan Blot <tsuku0611@protonmail.com>
2024-02-05 20:51:57 +0100

    Add Firebase to project

---
+++ Plugins/FirebaseGoodies/Source/FirebaseGoodies/Private/CloudStorage/Callbacks/FGCloudStorageDownloadInMemoryCallback.cpp
@@ -0,0 +1,39 @@
+﻿// Copyright (c) 2023 Nineva Studios
+
+#include "FGCloudStorageDownloadInMemoryCallback.h"
+
+#include "Async/Async.h"
+
+UFGCloudStorageDownloadInMemoryCallback::UFGCloudStorageDownloadInMemoryCallback()
+{
+	if (StaticClass()->GetDefaultObject() != this)
+	{
+		AddToRoot();
+	}
+}
+
+void UFGCloudStorageDownloadInMemoryCallback::BindSuccessDelegate(const FCloudStorageByteArrayDelegate& OnSuccess)
+{
+	OnDownloadInMemorySuccessDelegate = OnSuccess;
+}
+
+void UFGCloudStorageDownloadInMemoryCallback::BindErrorDelegate(const FCloudStorageStringDelegate& OnError)
+{
+	OnDownloadInMemoryErrorDelegate = OnError;
+}
+
+void UFGCloudStorageDownloadInMemoryCallback::ExecuteSuccess(TArray<uint8> ByteArray)
+{
+	AsyncTask(ENamedThreads::GameThread, [=]() {
+		OnDownloadInMemorySuccessDelegate.ExecuteIfBound(ByteArray);
+		this->RemoveFromRoot();
+	});
+}
+
+void UFGCloudStorageDownloadInMemoryCallback::ExecuteError(FString ErrorMessage)
+{
+	AsyncTask(ENamedThreads::GameThread, [=]() {
+		OnDownloadInMemoryErrorDelegate.ExecuteIfBound(ErrorMessage);
+		this->RemoveFromRoot();
+	});
+}
---
+++ Plugins/FirebaseGoodies/Source/FirebaseGoodies/Private/IOS/Utils/FGCloudFunctionsErrorUtils.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2022 Nineva Studios
+
+#pragma once
+
+#import <Foundation/Foundation.h>
+
+namespace CloudFunctionsErrorUtils
+{
+FString HandleError(NSError* error);
+}
